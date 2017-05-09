48a744fa1e7af74a61fa734cd887f784e6b74163
Ethan Hall <ethall@linkedin.com>
2017-05-09 14:40:07 -0700

    Added license files

--- plugin-portal-resources/src/main/java/com/linkedin/portal/resources/PluginResourceResource.java
+++ plugin-portal-resources/src/main/java/com/linkedin/portal/resources/PluginResourceResource.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2017 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.portal.resources;
 
 import com.linkedin.portal.model.RepositoryType;
--- webapp/src/main/java/com/linkedin/portal/webapp/Application.java
+++ webapp/src/main/java/com/linkedin/portal/webapp/Application.java
@@ -1,18 +1,19 @@
+/**
+ * Copyright 2017 LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.portal.webapp;
 
-import com.linkedin.portal.resources.config.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.linkedin.portal")
 @EnableJpaRepositories("com.linkedin.portal.resources.dao")
 @EntityScan("com.linkedin.portal.resources.dao")
-//@Import(ResourceConfig.class)
 public class Application {
 
     public static void main(String[] args) {
